f57eeca11946d8ade6f7f5ce70127c22f59dd7c6
prachishelke1312 <prachishelake2905@gmail.com>
2026-06-12 22:32:27 +0530

    fix package name in version test

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,5 +2,5 @@ import arc
 from importlib.metadata import version
 
 def test_version():
-    metadata_version = version("arc")
+    metadata_version = version("arc-training")
     assert arc.__version__ == metadata_version
